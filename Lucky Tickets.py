9598f2ea1388df9341be70246946cff560adac3a
Artemii Yanushevskyi <awaken@Artemiis-MBP.Dlink>
2018-07-03 12:30:27 +0300

    readme

---
+++ Lucky Tickets.py
@@ -0,0 +1,34 @@
+string = input("n suma: ")
+# convert input to integers
+n, suma = map(int, string.split(" "))
+
+def num_sum(number, n):
+    """ 4256 -> 17
+    """
+    num_sum = 0
+    for i in range(n, 0, -1):
+        rest = number % 10
+        num_sum += rest
+        number = (number - rest) // 10
+    return num_sum
+
+def num_to_str(number, n):
+    """ 2309 -> "002309"
+    """
+    number = str(number)
+    zeros = "0" * (n - len(number))
+    return zeros + number
+    
+suma_part = suma / 2 # suma must be even
+
+parts = []
+
+for i in range(10**n):
+    if suma_part == num_sum(i, n):
+        # select the parts that have suma_part sum of its digits
+        parts.append(num_to_str(i, n))
+
+for p1 in parts:
+    for p2 in parts:
+        # combine the parts in all possible ways
+        print(p1+p2)
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # OdooTestTask
+### PYTHON 3.6
+
+## File Tracker
+###### EXERCISE 6
+
+Program tracks changes in given folder. In case there is new file added, program outputs its name. Based on resolution of a new file, program handles the file in a way that is provided in ```FILE_TRACKER_CONFIG.py``` .
+
+The five actions are available:
+
+```'trash',
+'nothing',
+'add-date-to-name',
+'copy-to-folder path/to/folder',
+'move-to-folder path/to/folder'```
+
+
+
+
+
+
+
+
